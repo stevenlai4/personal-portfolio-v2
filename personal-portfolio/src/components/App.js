d0ff1b9d6adb70e366d9c3a4d5fdd9a83be667ae
Steven <clai86@my.bcit.ca>
2020-11-18 19:28:56 -0800

    Add favicon & clean code

---
+++ personal-portfolio/src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.scss';
+
+function App() {
+    return (
+        <div className="App container">
+            <h1>Hello</h1>
+        </div>
+    );
+}
+
+export default App;
